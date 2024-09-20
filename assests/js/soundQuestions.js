f1c5d0636e9f6eac371ec6c33ab9a94871932334
Kiree Bellamy <kireebellamy@gmail.com>
2024-09-20 11:10:39 +0000

    updating questions

--- assests/js/soundQuestions.js
+++ assests/js/soundQuestions.js
@@ -5,6 +5,44 @@ export const soundQuestions = [
         question: "Identify the instrument.",
         options: ["Maracas", "Cowbell", "Claves", "Triangle"],
         answer: "Cowbell",
-        sound: "https://splice.com/sounds/sample/1795c7fdff35b1716a8505c39c301d3ad5ca0e7da55771c541ce572b2b13115f"  // Sound URL
+        sound: "https://splice.com/sounds/sample/1795c7fdff35b1716a8505c39c301d3ad5ca0e7da55771c541ce572b2b13115f"
       },
+
+    {
+        question: "Identify the instrument.",
+        options: ["Ukulele", "Steel Guitar", "Violin", "Banjo"],
+        answer: "Banjo",
+        sound: "https://splice.com/sounds/sample/a513cbeab5fc4b06b60dde53f511e88e47b48c8442f0b95f4b2cb3b038a9ed94"
+    },
+    {
+        question: "Identify the instrument.",
+        options: ["Kazoo", "Slide Whistle", "Recorder", "Harmonicas"],
+        answer: "Kazoo",
+        sound: "https://splice.com/sounds/sample/fc31d08ba4e776b68904ce65eca4f80ef9fa4a8d44e487ed2f6bff2f1bc3eef0"
+    },
+    {
+        question: "Identify the instrument.",
+        options: ["Obeo", "Saxaphone", "Clarinet", "Trumpet"],
+        answer: "Clarinet",
+        sound: "https://splice.com/sounds/sample/cb7ee21139af3f1a983de38994df447438908bff1f02c9fae705b87ee79377fc"
+    },
+    {
+        question: "Identify the instrument.",
+        options: ["Didgeridoo", "Trumpet", "Bongos", "Taiko"],
+        answer: "Didgeridoo",
+        sound: "https://splice.com/sounds/sample/fbd64852c397f4d9e9dd1ce4d078ac2e58c3abc356f576c603204465449e818d"
+    },
+    {
+        question: "Identify the instrument.",
+        options: ["FLute", "Pan Flute", "piccolo", "Bass Drum"],
+        answer: "Pan Flute",
+        sound: "https://splice.com/sounds/sample/6ccc3fc37221471ef3186e98abbb0d7c09a367a130cfd8abb7f76856b5ff4e37"
+    },
+    {
+        question: "Identify the instrument.",
+        options: ["French Horn", "Trombone", "Piano", "Trumpet"],
+        answer: "Trumpet",
+        sound: "https://splice.com/sounds/sample/37c4cf79fa01e9d293d308c722dd0faa71692da7f43121d9ec6ad09df040e720"
+    },
+    
 ]
