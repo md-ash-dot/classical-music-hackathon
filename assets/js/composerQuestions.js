47d6576770ba1e395d7d94d77bd8c08ed4b82fcb
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2024-09-20 19:33:28 +0100

    add composer questions

---
+++ assets/js/composerQuestions.js
@@ -0,0 +1,67 @@
+// Define an array to hold your quiz questions
+export const popCultureQuestions = [
+    // Add questions here
+    {
+        question: "This composer wrote 'The Four Seasons'.",
+        options: ["Mozart", "Bach", "Vivaldi", "Handel"],
+        answer: "Vivaldi",
+        hint: "This Italian Baroque composer was nicknamed 'The Red Priest'."
+    },
+    {
+        question: "Known for his operas, this Italian composer wrote 'La Traviata' and 'Rigoletto'.",
+        options: ["Puccini", "Verdi", "Rossini", "Donizetti"],
+        answer: "Verdi"
+    },
+    {
+        question: "Known as the 'Father of the Symphony', this composer was also completely deaf in his later years.",
+        options: ["Beethoven", "Haydn", "Schubert", "Mahler"],
+        answer: "Beethoven"
+    },
+    {
+        question: "This Baroque composer wrote the Brandenburg Concertos.",
+        options: ["Handel", "Vivaldi", "Telemann", "Bach"],
+        answer: "Bach"
+    },
+    {
+        question: "This Russian composer is famous for 'The Nutcracker' ballet.",
+        options: ["Stravinsky", "Rachmaninoff", "Tchaikovsky", "Prokofiev"],
+        answer: "Tchaikovsky"
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    
+];
