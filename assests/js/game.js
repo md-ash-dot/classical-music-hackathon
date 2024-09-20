0ff3d162a2e506e89b61d4a9d6edf30a71a659d3
Kiree Bellamy <kireebellamy@gmail.com>
2024-09-20 08:28:16 +0000

    Added separate .js for logiand questions, css, and meta for html

---
+++ assests/js/game.js
@@ -0,0 +1,4 @@
+// Importing the questions array
+import {instrumentQuestions} from "./instrumentQuestions.js";
+import {soundQuestions} from "./soundQuestions.js";
+import {popCultureQuestions} from "./popCultureQuestions.js";
