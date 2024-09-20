cfd02861163fa3cbb557690722ca7efec5a3c048
Kiree Bellamy <kireebellamy@gmail.com>
2024-09-20 15:02:59 +0000

    Updating audio media and pop culture

--- assets/js/popCultureQuestions.js
+++ assets/js/popCultureQuestions.js
@@ -1,5 +1,124 @@
 // Define an array to hold your quiz questions
 export const popCultureQuestions = [
     // Add questions here
-
-]
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+    {
+        question: "Which of these movies featured:",
+        options: ["Apocalypse now", "Platoon", "Born on the 4th of July", "The Longest Day"],
+        answer: "Platoon",
+        sound: "https://upload.wikimedia.org/wikipedia/en/d/de/Adagio_for_Strings-Samuel_Barber.ogg"
+    },
+    {
+        question: "Which of these movies featured:",
+        options: ["2001: A Space Odyssey", "Alien", "Interstellar", "Planet of The Apes"],
+        answer: "2001: A Space Odyssey",
+        sound: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Richard_Strauss_-_Also_Sprach_Zarathustra.ogg"
+    },
+    {
+        question: "Which of these movies featured:",
+        options: ["Rocky", "Saving Private Ryan", "Tales of The City", "Philadelphia"],
+        answer: "Philadelphia",
+        sound: "../assets/audio/La-morta.mp3"
+    },
+    {
+        question: "Which of these movies featured:",
+        options: ["One Flew Over the Cuckoo's Nest", "Five Easy Pieces", "A Clockwork Orange", "The Graduate"],
+        answer: "Five Easy Pieces",
+        sound: "../assets/audio/chopin.mp3"
+    },
+    {
+        question: "Which of these movies featured:",
+        options: ["", "", "", "Brief Encounters"],
+        answer: "",
+        sound: "../assets/audio/rachmaninov.mp3"
+    },
+    {
+        question: "",
+        options: ["", "", "", ""],
+        answer: "",
+        sound: ""
+    },
+];
