5f4fd5013230e3ef3b29cc6982cc361ba9ee0f48
irasan <iryna.sanzhara@gmail.com>
2024-09-22 17:50:05 +0200

    Update instrument questions

--- assets/js/instrumentQuestions.js
+++ assets/js/instrumentQuestions.js
@@ -17,11 +17,6 @@ const instrumentQuestions = [
         answer: "Harp",
     },
     {
-        question: "Which voice type is the lowest in the male vocal range?",
-        options: ["Baritone", "Tenor", "Bass", "Countertenor"],
-        answer: "Bass",
-    },
-    {
         question: "What is the term for a piece of music written for a solo instrument with orchestral accompaniment?",
         options: ["Sonata", "Concerto", "Symphony", "Opera"],
         answer: "Concerto",
@@ -32,34 +27,9 @@ const instrumentQuestions = [
         answer: "Brass",
     },
     {
-        question: "In a classical music concert, who typically stands at the front of the orchestra and leads the musicians?",
-        options: ["Leader", "Music Master", "Timekeeper", "Conductor"],
-        answer: "Conductor",
-    },
-    // {
-    //     question: "In which type of musical composition is a soloist featured alongside an orchestra?",
-    //     options: ["Symphony", "Opera", "Concerto", "Fuego"],
-    //     answer: "Concerto",
-    // },
-    {
-        question: "What is the term for a repeated musical phrase or rhythm in classical music?",
-        options: ["Repato", "Ostinato", "Arpeggio", "Legato"],
-        answer: "Ostinato",
-    },
-    {
-        question: "What is a 'movement' in classical music?",
-        options: ["A group of musicians", "A distinct section of a larger musical work", "A change in the speed of the music", "A style of conducting"],
-        answer: "A distinct section of a larger musical work",
-    },
-    {
         question: "Which of the following instruments is NOT a woodwind instrument?",
         options: ["Clarinet", "Flute", "Trumpet", "Oboe"],
         answer: "Trumpet",
-    },
-    {
-        question: "What is the highest female singing voice called?",
-        options: ["Soprano", "Tenor", "Alto", "Bass"],
-        answer: "Soprano",
     },
     {
         question: "Which percussion instrument do you typically hit with mallets?",
